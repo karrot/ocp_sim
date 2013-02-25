f039893ea6e0f6fd3b765c6f00b6c9b48e456b74
Eric <epicmusicdj@gmail.com>
2013-02-24 21:35:18 -0500

    Create ocp.py

---
+++ ocp.py
@@ -0,0 +1,44 @@
+import random
+import time
+
+#writing to txt function
+def writing():
+    f = open( 'results_1.txt', 'a' )
+    f.write('player1 '+repr(player1)+'\n')
+    f.write('player2 '+repr(player2)+'\n')
+    f.write('\n')
+    f.close()
+
+# Now your sampling bit
+rank1 = (10,10.5,12,12.5)
+#10=10s
+#10.5=10h
+#12=Qs
+#12.5=Qh
+coinflip=(1,2)
+player1=100
+player2=100
+
+#x is player 1
+#y is player 2
+#optimal bluffing
+
+sample=int(raw_input("Enter sample size: "))
+for i in range(0,sample):
+    x=random.choice(rank1)
+    if x==12 or x==12.5:
+        player1=player1+3
+        player2=player2-3
+            #he calls value bet no matter what - value bet of $2 gives 2:1 odds
+            #twice as many value bets as bluffs
+            #GTO must make 50 vb (queens), 25b, 25f (10s)
+        
+    if x==10:
+        player1=player1-3
+        player2=player2+3
+    if x==10.5:
+        player1=player1-1
+        player2=player2+1
+    print player1
+    print player2
+    writing()
